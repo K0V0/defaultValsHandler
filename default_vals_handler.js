3ac1436e86c0f9fdf7c9fd2e0ac4e6f8bc5fbaef
Matej Kováč <kovo@kovo-notebook>
2016-04-12 00:45:59 +0200

    fixed-added support for elements that are dynamically created

--- default_vals_handler.js
+++ default_vals_handler.js
@@ -1,5 +1,41 @@
 (function($) {
 	$.fn.defaultValsHandler = function () {
+
+		function getSelector(el) {
+		    var $el = $(el);
+
+		    var id = $el.attr("id");
+		    if (id) { 
+		        return "#"+ id;
+		    }
+
+		    var selector = $el.parents()
+		                .map(function() { return this.tagName; })
+		                .get().reverse().join(" ");
+
+		    if (selector) {
+		        selector += " "+ $el[0].nodeName;
+		    }
+
+		    var classNames = $el.attr("class");
+		    if (classNames) {
+		        selector += "." + $.trim(classNames).replace(/\s/gi, ".");
+		    }
+
+		    var name = $el.attr('name');
+		    if (name) {
+		        selector += "[name='" + name + "']";
+		    }
+		    if (!name){
+		        var index = $el.index();
+		        if (index) {
+		            index = index + 1;
+		            selector += ":nth-child(" + index + ")";
+		        }
+		    }
+		    return selector;
+		}
+
 		return this.each(function() {
 
 			var val_getter_type;
@@ -9,7 +45,9 @@
 				val_getter_type = "text";
 			}
 
-			$(this).on('focus', function() {
+			var selector = getSelector($(this));
+
+			$(document).on('focus', selector, function() {
 				var dv = $(this).data('default-val');
 				var v = $(this)[val_getter_type]();
 				if ((dv === undefined)||(dv == v)) {
@@ -18,7 +56,7 @@
 				}
 			});
 
-			$(this).focusout(function() {
+			$(document).on('focusout', selector, function() {
 				var d;
 				if ((d = $(this).data('default-val')) !== undefined) {
 					if ($(this)[val_getter_type]() == '') {
